f01044d5d3052a873b65880e40afd82089774caf
Alex <awarrington0895@gmail.com>
2023-04-23 20:24:19 +0000

    Install and init chezmoi

--- install.sh
+++ install.sh
@@ -2,3 +2,9 @@
 git config --global alias.co checkout
 git config --global alias.s status
 git config --global alias.fa fetch --all
+
+# Install Chezmoi
+sh -c "$(curl -fsLS get.chezmoi.io)"
+
+# Initialize Chezmoi 
+chezmoi init https://github.com/awarrington0895/dotfiles.git
